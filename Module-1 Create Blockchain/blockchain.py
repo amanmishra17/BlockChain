43cbe0fe0f0f0737c8e2d1c2709f7e8a90c0bbe7
Aman Kumar Mishra <aman.klmishra1715.am@gmail.com>
2019-10-13 16:25:06 +0530

    functions added to the class blockchain and def contents of the block

--- Module-1 Create Blockchain/blockchain.py
+++ Module-1 Create Blockchain/blockchain.py
@@ -22,6 +22,7 @@ class Blockchain:
                  'previous_hash': previous_hash}
         self.chain.append(block)
         return block
-
+    def get_previous_block(self):
+        return self.chain[-1]
 
  
